bfee83c7630642f9a8f8774e3c8b27fce9f1c336
Adam Scott <adam.scott@annotell.com>
2019-11-30 16:02:46 +0100

    Updates

---
+++ precache-manifest.ae1d0fefeadfb31350641e5fefcdba3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c594e8cd256c71da7a3411852dd02acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce24cea1d38bfe24a0c3",
+    "url": "/static/css/main.44dbd637.chunk.css"
+  },
+  {
+    "revision": "1c372d19dfd7773ee35e",
+    "url": "/static/js/2.bc2abb5e.chunk.js"
+  },
+  {
+    "revision": "ce24cea1d38bfe24a0c3",
+    "url": "/static/js/main.49c51878.chunk.js"
+  },
+  {
+    "revision": "c493e35639632e55756f",
+    "url": "/static/js/runtime~main.8d9d9985.js"
+  },
+  {
+    "revision": "4a3f2928d5b01a9dd5eb2d2e982fe18c",
+    "url": "/static/media/EstiloPro-Light.4a3f2928.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adamjosefin/precache-manifest.6b772f04721567c7de62a83e31fe572a.js"
+  "/precache-manifest.ae1d0fefeadfb31350641e5fefcdba3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adamjosefin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
