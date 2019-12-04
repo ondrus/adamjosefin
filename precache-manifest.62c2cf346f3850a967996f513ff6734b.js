6bb19dee029240b02d7c35191c0a7e77d817b2bf
ondrus <josefin.ondrus@gmail.com>
2019-12-04 23:51:00 +0100

    Updates

---
+++ precache-manifest.62c2cf346f3850a967996f513ff6734b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b338421d1232e7278b52d5b5dc8597f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c76d1d352da7f10b1c94",
+    "url": "/static/css/main.af19e68f.chunk.css"
+  },
+  {
+    "revision": "16e244fed4c935a8cdf0",
+    "url": "/static/js/2.c1e72f34.chunk.js"
+  },
+  {
+    "revision": "c76d1d352da7f10b1c94",
+    "url": "/static/js/main.438cf096.chunk.js"
+  },
+  {
+    "revision": "c493e35639632e55756f",
+    "url": "/static/js/runtime~main.8d9d9985.js"
+  },
+  {
+    "revision": "4a3f2928d5b01a9dd5eb2d2e982fe18c",
+    "url": "/static/media/EstiloPro-Light.4a3f2928.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adamjosefin/precache-manifest.062ec1861d45169a6ade5a567ffda07c.js"
+  "/precache-manifest.62c2cf346f3850a967996f513ff6734b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adamjosefin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
